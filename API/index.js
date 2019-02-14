1c48fc5cca5b806380f3a00b27f0922aa72ae410
Olamide <aboyejiolamide15@gmail.com>
2019-02-15 00:58:36 +0100

    updated index.js to adhere to style guide

--- API/index.js
+++ API/index.js
@@ -7,4 +7,6 @@ app.get('*', (req, res) => {
   res.json('Welcome to my world');
 });
 
-app.listen(port, () => console.log(`working ${port}`));
+app.listen(port, () => {
+  console.log(`server working on port ${port}`);
+}
