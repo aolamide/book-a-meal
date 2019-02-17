64ea710fd2bd821694ae53cc60b4514f1566faa3
Olamide <aboyejiolamide15@gmail.com>
2019-02-17 22:24:55 +0100

    dealt with mobile responsiveness and fixed some bugs

--- UI/admin/addmeal.js
+++ UI/admin/addmeal.js
@@ -1,11 +1,38 @@
 const addButton = document.getElementById('add-btn');
-const Modal = document.getElementById('modal');
+const modal = document.getElementById('modal');
 const closeModal = document.getElementById('close-modal');
+const deleteMeal = document.querySelectorAll('.btn-delete');
+const modifyMeal = document.querySelectorAll('.btn-modify');
+const name = document.getElementById('name');
+const price = document.getElementById('price');
+const link = document.getElementById('link');
 
 addButton.addEventListener('click', () => {
-    Modal.style.display = 'block';
+  modal.style.display = 'block';
 });
 
 closeModal.addEventListener('click', () => {
-    Modal.style.display = 'none';
+  modal.style.display = 'none';
+  name.value = '';
+  price.value = '';
+  link.value = '';
 });
+
+deleteMeal.forEach((button) => {
+	button.addEventListener('click', () => {
+		let mealDiv = button.parentNode.parentNode
+		mealDiv.parentNode.removeChild(mealDiv);
+	});
+})
+
+modifyMeal.forEach((button) => {
+	button.addEventListener('click', () => {
+		modal.style.display = 'block';
+		const parent = button.parentNode;
+		const priceValue = parent.previousElementSibling;
+		const nameValue = priceValue.previousElementSibling;
+		name.value = nameValue.innerText;
+		price.value = priceValue.innerText;
+		link.value = 'https://placehold.it/355'
+	});
+})
